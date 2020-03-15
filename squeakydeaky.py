8c59da1f09e59e7fb4198b952666dfa1fdf727d0
chrisalexander <chris@chris-alexander.co.uk>
2020-03-15 17:45:56 +0000

    python3

--- squeakydeaky.py
+++ squeakydeaky.py
@@ -1,9 +1,12 @@
 if __name__ == '__main__':
-    import systemd.daemon, initio, redis
+    import systemd.daemon
+    import initio
+    import redis
 
     print('Startup')
     initio.init()
-    r = redis.Redis(host='192.168.0.1', port=6379, db=0)
+    r = redis.Redis(host='192.168.0.1', port=6379,
+                    db=0, decode_responses=True)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe('squeaky')
     print('Startup complete')
