779d4fcd64fb14aba6e53e84a415dccb31d9198a
chrisalexander <chris@chris-alexander.co.uk>
2020-03-16 20:28:30 +0000

    python tidy-up

--- squeakydeaky.py
+++ squeakydeaky.py
@@ -1,22 +1,28 @@
-if __name__ == '__main__':
-    import systemd.daemon
-    import initio
-    import redis
+import systemd.daemon
+import initio
+import redis
 
+
+def execute():
     print('Startup')
+
     initio.init()
     r = redis.Redis(host='192.168.0.1', port=6379,
                     db=0, decode_responses=True)
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe('squeaky')
+
     print('Startup complete')
     systemd.daemon.notify('READY=1')
 
     try:
         for message in p.listen():
-            # Don't need to read the contents of the message, it doesn't matter
-            r.publish("deaky", initio.getDistance())
+            r.publish('deaky', initio.getDistance())
     except:
         p.close()
         initio.cleanup()
-        print("Goodbye")
+        print('Goodbye')
+
+
+if __name__ == '__main__':
+    execute()
